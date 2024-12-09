29befeb963c8df9fb2a5651f9696ec8e74730256
dalnikcc <dalnikcc@gmail.com>
2024-12-09 16:00:05 +0100

    Alternative uppercase way

--- htaccess.php
+++ htaccess.php
@@ -3,7 +3,20 @@ RewriteEngine On
 RewriteCond %{REQUEST_URI} ^(.*)//(.*)$
 RewriteRule . %1/%2 [R=301,L]
 
+--------------
+
 # Redirecting from uppercase to lowercase links
 RewriteMap lc int:tolower
 RewriteCond %{REQUEST_URI} [A-Z]
 RewriteRule (.*) ${lc:$1} [R=301,L]
+
+OR
+
+
+add_action('template_redirect', function () {
+    $url = $_SERVER['REQUEST_URI'];
+    if (preg_match('/[A-Z]/', $url)) { 
+        wp_redirect(home_url(strtolower($url)), 301);
+        exit;
+    }
+});
