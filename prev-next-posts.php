04f7c6f613c04e7140d9cec0db2673f78ceb6948
dalnikcc <dalnikcc@gmail.com>
2025-11-01 16:24:49 +0100

    Create prev-next-posts.php

---
+++ prev-next-posts.php
@@ -0,0 +1,22 @@
+<?php
+$prev = get_previous_post();
+$next = get_next_post();
+?>
+
+<?php if ( $prev ): ?>
+<a href="<?= esc_url( get_permalink( $prev ) ); ?>" class="prev-btn" rel="prev"> 
+  <div class="btn-content">
+    <span class="para"><?= pll__('Previous post') ?></span>
+    <h4 class="title"><?= esc_html( get_the_title( $prev ) ); ?></h4>
+  </div>
+</a>
+<?php endif; ?>
+
+<?php if ( $next ): ?>
+<a href="<?= esc_url( get_permalink( $next ) ); ?>" class="next-btn" rel="next">
+  <div class="btn-content">
+    <span class="para"><?= pll__('Next post') ?></span>
+    <h4 class="title"><?= esc_html( get_the_title( $next ) ); ?></h4>
+  </div> 
+</a>
+<?php endif; ?>
